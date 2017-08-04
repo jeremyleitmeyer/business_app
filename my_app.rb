d52e138a947ad743237beea615077e5c326cddb2
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-08-04 14:48:21 -0400

    label pages

--- my_app.rb
+++ my_app.rb
@@ -4,5 +4,17 @@ class MyApp < Sinatra::Base
   set :public_folder => "public", :static => true
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  get "/team" do
+    erb :team
+  end
+
+  get "/product" do
+    erb :product
+  end
+
+  get "/contact" do
+    erb :contact
   end
